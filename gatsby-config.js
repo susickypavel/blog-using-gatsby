4668598206f715c506ce3a01d151d9953a804bea
Pavel Sušický <pavels.sus@gmail.com>
2019-09-01 10:19:57 +0200

    feat: 🎸 add gatsby plugin manifest for pwa

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,16 @@
 module.exports = {
-  plugins: [`gatsby-plugin-typescript`]
+  plugins: [
+    `gatsby-plugin-typescript`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Pavel Susicky's blog",
+        short_name: "PS's blog",
+        start_url: "/",
+        background_color: "#000",
+        theme_color: "#000",
+        display: "standalone"
+      }
+    }
+  ]
 }
