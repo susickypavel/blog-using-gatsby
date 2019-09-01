55c929120ebf7101f71f7dad28a1b9cdddfda676
Pavel Sušický <pavels.sus@gmail.com>
2019-09-01 19:20:06 +0200

    feat: 🎸 add react helmet plugin for gatsby.js
    extend opengraph and twitter meta tags

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  plugins: [`gatsby-plugin-typescript`],
+  plugins: [`gatsby-plugin-typescript`, `gatsby-plugin-react-helmet`],
   siteMetadata: {
     description: "Personal blog of Paul from Czech",
     twitter: "@Thesoreon"
