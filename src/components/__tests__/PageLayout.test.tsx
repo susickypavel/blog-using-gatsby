018ad1098bbe1b26bf76574402eca9580ec614c7
Pavel Sušický <pavels.sus@gmail.com>
2019-09-01 19:30:15 +0200

    fix: 🐛 fix test for `PageLayout.tsx` component

--- src/components/__tests__/PageLayout.test.tsx
+++ src/components/__tests__/PageLayout.test.tsx
@@ -3,6 +3,8 @@ import { render } from "@testing-library/react"
 
 import PageLayout from "@components/PageLayout"
 
+jest.mock("@components/Seo", () => () => <div />)
+
 describe("PageLayout component", () => {
   test("should render children of PageLayout component", () => {
     const { container } = render(<PageLayout>Hello World</PageLayout>)
