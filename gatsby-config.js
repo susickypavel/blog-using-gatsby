96f0de6f81a472eeaca171a744177286836eeb56
Pavel Sušický <pavels.sus@gmail.com>
2019-09-04 20:34:35 +0200

    feat: 🎸 add emotion core libraries along with gatsby plugin
    setup Global styles with global css reset

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  plugins: [`gatsby-plugin-typescript`, `gatsby-plugin-react-helmet`],
+  plugins: [`gatsby-plugin-typescript`, `gatsby-plugin-react-helmet`, `gatsby-plugin-emotion`],
   siteMetadata: {
     description: "Personal blog of Paul from Czech",
     twitter: "@Thesoreon"
