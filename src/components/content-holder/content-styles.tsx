b86ea56d376ca7887496403fd79d165dbeb1ec6a
Pavel Sušický <pavels.sus@gmail.com>
2019-09-28 10:33:53 +0200

    fix: 🐛 sidebar layout for smaller devices

--- src/components/content-holder/content-styles.tsx
+++ src/components/content-holder/content-styles.tsx
@@ -1,8 +1,13 @@
 import styled from "@utils/styled"
+import { LAYOUT_MEDIA_QUERY_BP } from "@css/constants"
 
 export const ContentWrapper = styled.div`
   flex: 1 0 61%;
   height: 100%;
   background: ${props => props.theme.bg.primary};
   color: ${props => props.theme.color.primary};
+
+  @media screen and (max-width: ${LAYOUT_MEDIA_QUERY_BP}) {
+    flex: 1 0;
+  }
 `
--- src/components/main-sidebar/sidebar.tsx
+++ src/components/main-sidebar/sidebar.tsx
@@ -2,13 +2,19 @@ import React from "react"
 
 import Avatar from "@static/avatar.jpg"
 
-import { SidebarWrapper, SidebarAvatar, SidebarTitle } from "./sidebar-styles"
+import { SidebarWrapper, SidebarAvatar, SidebarTitle, SidebarHeaderHolder } from "./sidebar-styles"
+import Navigation from "./navigation/navigation"
+import SocialMediaHolder from "./social-media-links/social-media-holder"
 
 const SideBar: React.FC = () => {
   return (
     <SidebarWrapper>
-      <SidebarAvatar src={Avatar} />
-      <SidebarTitle>Pavel Sušický</SidebarTitle>
+      <SidebarHeaderHolder>
+        <SidebarAvatar src={Avatar} />
+        <SidebarTitle>Pavel Sušický</SidebarTitle>
+      </SidebarHeaderHolder>
+      <Navigation />
+      <SocialMediaHolder />
     </SidebarWrapper>
   )
 }
