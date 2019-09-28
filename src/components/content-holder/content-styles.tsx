73d0790a53decac8725fcc61bec79daea6e5ceba
Pavel Sušický <pavels.sus@gmail.com>
2019-09-28 15:27:17 +0200

    feat: 🎸 edit styles of sidebar and content holder

--- src/components/content-holder/content-styles.tsx
+++ src/components/content-holder/content-styles.tsx
@@ -1,13 +1,14 @@
 import styled from "@utils/styled"
+import { getGoldenRatio } from "@utils/golden-ratio"
 import { LAYOUT_MEDIA_QUERY_BP } from "@css/constants"
 
 export const ContentWrapper = styled.div`
-  flex: 1 0 61%;
-  height: 100%;
+  width: ${getGoldenRatio(100, "A")}%;
+  min-height: 100vh;
   background: ${props => props.theme.bg.primary};
   color: ${props => props.theme.color.primary};
 
   @media screen and (max-width: ${LAYOUT_MEDIA_QUERY_BP}) {
-    flex: 1 0;
+    width: 100%;
   }
 `
--- src/components/main-sidebar/sidebar-styles.tsx
+++ src/components/main-sidebar/sidebar-styles.tsx
@@ -1,4 +1,5 @@
 import styled from "@utils/styled"
+import { getGoldenRatio } from "@utils/golden-ratio"
 
 import {
   LAYOUT_MEDIA_QUERY_BP,
@@ -8,26 +9,35 @@ import {
 } from "@css/constants"
 
 export const SidebarWrapper = styled.div`
-  flex: 1 0 39%;
-  background: ${props => props.theme.bg.secondary};
   color: ${props => props.theme.color.primary};
-  border-left: 1px solid black;
 
-  padding: ${SIDEBAR_PADDING};
+  width: ${getGoldenRatio(100, "B")}%;
+  height: 100%;
 
   display: flex;
   flex-flow: column wrap;
-  align-items: center;
+
+  position: fixed;
+  right: 0;
+
+  padding: ${SIDEBAR_PADDING};
+  text-align: center;
+
+  background-color: ${props => props.theme.bg.secondary};
 
   @media screen and (max-width: ${LAYOUT_MEDIA_QUERY_BP}) {
-    flex-grow: 0;
-    border-left: none;
+    width: 100%;
+    height: auto;
+    position: static;
   }
 `
 
 export const SidebarAvatar = styled.img`
   width: 100px;
   height: auto;
+
+  display: block;
+  margin: 0 auto;
 
   border-radius: 100%;
 `
@@ -36,9 +46,4 @@ export const SidebarTitle = styled.p`
   font-size: ${SIDEBAR_TITLE_FONTSIZE};
   font-weight: bold;
   margin-top: ${SIDEBAR_TITLE_MARGINTOP};
-`
-
-export const SidebarHeaderHolder = styled.div`
-  width: 100%;
-  text-align: center;
 `
