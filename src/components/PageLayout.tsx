86f78690dbe855b84814a623756d1aab02daf064
Pavel Sušický <pavels.sus@gmail.com>
2019-10-05 09:49:46 +0200

    feat: 🎸 add custom components for elements in blog posts

--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { Global } from "@emotion/core"
+import { MDXProvider } from "@mdx-js/react"
 
 import Seo from "./Seo"
 import globalStyles from "@css/global-styles"
@@ -9,18 +10,34 @@ import ContentHolder from "./content-holder/content-holder"
 import SideBar from "./main-sidebar/sidebar"
 import SiteThemeProvider from "./theme-provider/theme-provider"
 
+import { BlockQuote, UnorderedList, OrderedList, Heading } from "./blog/md-components"
+
 interface Props {
   title?: string
 }
 
 const PageLayout: React.FC<Props> = ({ children, title = "Blog" }) => {
   return (
-    <SiteThemeProvider>
-      <Seo title={title} />
-      <Global styles={[globalStyles, resetStyles]} />
-      <ContentHolder>{children}</ContentHolder>
-      <SideBar />
-    </SiteThemeProvider>
+    <MDXProvider
+      components={{
+        blockquote: props => <BlockQuote {...props} />,
+        ul: props => <UnorderedList {...props} />,
+        ol: props => <OrderedList {...props} />,
+        h1: props => <Heading {...props} heading="h1" fontSize={4} />,
+        h2: props => <Heading {...props} heading="h2" fontSize={3.2} />,
+        h3: props => <Heading {...props} heading="h3" fontSize={2.8} />,
+        h4: props => <Heading {...props} heading="h4" fontSize={2.6} />,
+        h5: props => <Heading {...props} heading="h5" fontSize={2.4} />,
+        h6: props => <Heading {...props} heading="h6" fontSize={2} />
+      }}
+    >
+      <SiteThemeProvider>
+        <Seo title={title} />
+        <Global styles={[globalStyles, resetStyles]} />
+        <ContentHolder>{children}</ContentHolder>
+        <SideBar />
+      </SiteThemeProvider>
+    </MDXProvider>
   )
 }
 
---
+++ src/components/blog/md-components/index.tsx
@@ -0,0 +1,58 @@
+import React from "react"
+import styled from "@utils/styled"
+import {
+  BLOCKQUOTE_PADDING,
+  BLOCKQUOTE_MARGIN,
+  BLOCKQUOTE_BORDERRADIUS,
+  BLOCKQUOTE_FONTSIZE
+} from "@css/constants"
+
+const BlockQuoteWrapper = styled.blockquote`
+  background: ${props => props.theme.bg.secondary};
+  padding: ${BLOCKQUOTE_PADDING};
+  margin: ${BLOCKQUOTE_MARGIN};
+  border-radius: ${BLOCKQUOTE_BORDERRADIUS};
+  font-size: ${BLOCKQUOTE_FONTSIZE};
+
+  border-left: 4px solid ${props => props.theme.color.primary};
+`
+
+export const UnorderedList = styled.ul`
+  list-style-type: inherit;
+  padding-left: 20px;
+  margin-top: 8px;
+
+  & li {
+    margin: 2px 0;
+  }
+`
+
+export const OrderedList = styled.ol`
+  list-style-type: decimal;
+  padding-left: 20px;
+  margin-top: 8px;
+
+  & li {
+    margin: 2px 0;
+  }
+`
+
+export const Heading: React.FC<{
+  heading: "h1" | "h2" | "h3" | "h4" | "h5" | "h6"
+  fontSize: number
+}> = ({ heading, fontSize, children }) => {
+  const HeadingComponent = styled(heading)`
+    font-size: ${fontSize}rem;
+    margin: 32px 0;
+  `
+
+  return <HeadingComponent>{children}</HeadingComponent>
+}
+
+export const BlockQuote: React.FC = ({ children }) => {
+  return (
+    <BlockQuoteWrapper>
+      <p>{children}</p>
+    </BlockQuoteWrapper>
+  )
+}
